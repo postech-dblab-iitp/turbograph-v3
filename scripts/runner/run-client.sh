6ca75a4bcf01af6c310ff7b6d832311078c805c9
Jaehyun Ha <rntlqvnf@naver.com>
2025-03-10 02:08:41 +0000

    Add Basic Query Generation'

--- scripts/runner/run-client.sh
+++ scripts/runner/run-client.sh
@@ -6,11 +6,10 @@ db_dir=$1
 
 ${BUILD_DIR}/client \
 	--log-level info \
-	--standalone \
 	--workspace ${db_dir} \
 	--log-level debug \
 	--standalone \
 	--disable-merge-join \
 	--join-order-optimizer exhaustive \
 	--profile \
-	--explain 
+	--explain \
